bb4fb6390f0e059e21c7473a39babe72ba9752b1
cReAtOR0009 <epidnugoairdrop@gmail.com>
2023-12-25 20:28:13 +0100

    added featured component

--- src/components/FeaturedProperties.jsx
+++ src/components/FeaturedProperties.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { featuredProducts } from "../assets/textAssets";
+import { featuredProducts, leftarrow, rightarrow } from "../assets/textAssets";
+import "../styles/featured.css"
 
 const FeaturedItemCard = ({image, title, details, features, price}) => {
   return (
@@ -22,7 +23,7 @@ const FeaturedItemCard = ({image, title, details, features, price}) => {
             ))}
           </div>
           <div className="featureditemPriceContainer" >
-            <div className="price"><span>Price:</span>{`$${price}`}</div>
+            <div className="price"><span>Price</span>{`$${price}`}</div>
             <div className="buyButton">
               <button>View Property Details</button>
             </div>
@@ -58,7 +59,16 @@ const FeaturedItemCard = ({image, title, details, features, price}) => {
                 <FeaturedItemCard key={index} {...featuredProduct} />
             ))}
         </div>
-        <div></div>
+        <div className="featuredToggle">
+            <p>
+                <span className="currentPage">01</span>of
+                <span className="totalPage">60</span>
+            </p>
+            <div className="toggleButton">
+                <div className="previous"><img src={leftarrow} alt="" /></div>
+                <div className="next"><img src={rightarrow} alt="" /></div>
+            </div>
+        </div>
       </div>
     </div>
   );
