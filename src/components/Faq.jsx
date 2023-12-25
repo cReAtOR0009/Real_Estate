093fe8dbc86270c755409c7d6521949ac1555000
cReAtOR0009 <epidnugoairdrop@gmail.com>
2023-12-25 20:27:19 +0100

    added an faq component

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from "react";
+import { faqs } from "../assets/textAssets";
+import { leftarrow, rightarrow } from "../assets/textAssets";
+import "../styles/faq.css"
+
+const FaqCard = ({question, answer, id}) => {
+  return (
+    <div className="faqCard">
+        <h3>{question}</h3>
+        <p>{answer}</p>
+        <button className="learnmore"><a href="#">Read More</a></button>
+    </div>
+  )
+}
 
 export const Faq = () => {
   return (
-    <div>Faq</div>
-  )
-}
+    <div className="faqContainer">
+      <div className="headerText">
+        <div className="left">
+          
+        <h2>Frequently Asked Questions</h2>
+        <p>
+        Find answers to common questions about Estatein's services, property listings, and the real estate process.   
+        </p>
+        </div>
+        <div className="right">
+          <button>View All FAQ's</button>
+        </div>
+      </div>
+      <div className="faqCardContainer">
+        {
+          faqs.map((faq, index) => (
+            <FaqCard key={index} {...faq} />
+          ))
+        }
+      </div>
+      <div className="featuredToggle">
+            <p>
+                <span className="currentPage">01</span>of
+                <span className="totalPage">60</span>
+            </p>
+            <div className="toggleButton">
+                <div className="previous"><img src={leftarrow} alt="" /></div>
+                <div className="next"><img src={rightarrow} alt="" /></div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+
+export default Faq
