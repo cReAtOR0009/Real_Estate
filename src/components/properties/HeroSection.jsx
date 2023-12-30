41beae75d499ef1cee2d6c05da638bf2e43d4e5c
cReAtOR0009 <epidnugoairdrop@gmail.com>
2023-12-30 13:35:45 +0100

    implemented usestate for form

--- src/components/properties/HeroSection.jsx
+++ src/components/properties/HeroSection.jsx
@@ -3,40 +3,52 @@ import { styles } from "../../styles/styles.js";
 import { searchinputs } from "../../assets/textAssets.js";
 import { searchIcon } from "../../assets/imageImporter.js";
 
-const HeroSection = () => {
-  const [searchParams, setsearchParams] = useState({Location:"", propertyType:"", calender:"", pricingRange:"", buildYear:""})
+const SearchParameterInput = ({
+  icon,
+  placeholder,
+  id,
+  name,
+  handleChange,
+  type,
+  searchBtn,
+}) => {
+  return (
+    <div className="flex align-middle w-[100%] relative px-[12px] py-[14px] border border-solid border-Grey-15 rounded-[8px]  bg-Grey-08">
+      <img className=" " src={icon} alt="" />
+      <input
+        type={type}
+        placeholder={placeholder}
+        id={id}
+        name={name}
+        onChange={(e) => handleChange(e, name)}
+        className="border-l border-solid border-Grey-15 ml-[8px] pl-[8px] text-left "
+      />
+      <button className="absolute bottom-50 right-5 md:right-[10px]">
+        <img src={searchBtn} alt="" />
+      </button>
+    </div>
+  );
+};
 
-  const handleChange = (e, name)=> {
-    setsearchParams(initialValue=> ({...initialValue, [name]:e.target.value}))
-    console.log(searchParams)
-  }
-  const SearchParameterInput = ({
-    icon,
-    placeholder,
-    id,
-    name,
-    value,
-    type,
-    searchBtn,
-  }) => {
-    return (
-      <div className="flex align-middle w-[100%] relative px-[12px] py-[14px] border border-solid border-Grey-15 rounded-[8px]  bg-Grey-08">
-        <img className=" " src={icon} alt="" />
-        <input
-          type={type}
-          placeholder={placeholder}
-          id={id}
-          name={name}
-          value={searchParams[name]}
-          onChange={(e) => handleChange(e, name)}
-          className="border-l border-solid border-Grey-15 ml-[8px] pl-[8px] text-left "
-        />
-        <button className="absolute bottom-50 right-5 md:right-[10px]">
-          <img src={searchBtn} alt="" />
-        </button>
-      </div>
-    );
+const HeroSection = () => {
+  const [searchParams, setsearchParams] = useState({
+    Location: "",
+    propertyType: "",
+    calender: "",
+    pricingRange: "",
+    buildYear: "",
+  });
+
+  console.log(searchParams);
+
+  const handleChange = (e, name) => {
+    setsearchParams((initialValue) => ({
+      ...initialValue,
+      [name]: e.target.value,
+    }));
+    console.log(searchParams);
   };
+
   return (
     <>
       <div className={styles.TextContainer}>
@@ -62,9 +74,22 @@ const HeroSection = () => {
         </div>
         <div className="flex flex-col sm:flex-wrap gap-[20px] justify-between mt-[20px] p-[20px] bg-Grey-10  rounded-[12px] md:flex-row lg:flex-nowrap md:mt-[-5px]">
           {searchinputs.map((searchinput, index) => (
-            <SearchParameterInput key={index} {...searchinput} />
+            <SearchParameterInput
+              key={index}
+              {...searchinput}
+              handleChange={handleChange}
+            />
           ))}
         </div>
+
+        <div className={styles.TextContainer}>
+        <h1 className={styles.heading}>Discover a World of Possibilities</h1>
+        <p className={styles.paragraph}>
+        Our portfolio of properties is as diverse as your dreams. Explore the following categories to find the perfect property that resonates with your vision of home{" "}
+        </p>
+      </div>
+
+        
       </div>
     </>
   );
