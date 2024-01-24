c3ba39acb271836a4ba9a70d821ba4fe07b5e1c6
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-01-24 15:53:31 +0100

    added db connection file

---
+++ backend/database/connection.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+    try {
+        let connectString = process.env.DB_URL;
+        if (process.env.DEV == "true") {
+            console.log('Development mode');
+            connectString = process.env.DB_URL_DEV;
+        }
+        console.log(connectString);
+        await mongoose.connect(connectString, {
+            useNewUrlParser: true,
+        });
+        console.log('Database connected')
+    } catch (error) {
+        console.log('Database connectivity error', error);
+        throw new Error(error);
+
+    }
+}
