53c4b24531a71fbb4290e97374d36b4d25e5989b
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-02-08 12:29:18 +0100

    renamed product controller to pro[erty controller

---
+++ backend/controller/propertyController.js
@@ -0,0 +1,132 @@
+const constants = require("../constants/index");
+// const propertyModel = require("../database/models/propertyModel");
+const Property = require("../database/models/propertyModel");
+const { formatMongoData, checkObjectId } = require("../utilities/formatData");
+const jwt = require("jsonwebtoken");
+
+const serverResponse = constants.serverResponse.defaultResponse;
+
+module.exports.createProperty = async (req, res) => {
+  let response = serverResponse;
+  try {
+    const {
+      name,
+      description,
+      price,
+      bedrooms,
+      bathrooms,
+      size,
+      address,
+      amenities,
+      additionalFeatures,
+      propertyType,
+      images,
+      tags,
+      status,
+      agent,
+      virtualTour,
+      propertyHistory,
+      nearbyAmenities,
+      availability,
+    } = req.body;
+
+    const existingPropertyName = await Property.findOne({ name: name });
+
+    if (existingPropertyName) {
+      throw new Error(
+        "this property already exist, kindly change prperty name "
+      );
+    }
+
+    // const {}
+
+    let newProperty = new Property({
+      name: name,
+      description: description,
+      price: price,
+      bedrooms: bedrooms,
+      bathrooms: bathrooms,
+      size: size,
+      address: address,
+      amenities: amenities,
+      additionalFeatures: additionalFeatures,
+      images: images,
+      propertyType: propertyType,
+      agent: agent,
+      tags: tags,
+      status: status,
+      virtualTour: virtualTour,
+      propertyHistory: propertyHistory,
+      nearbyAmenities: nearbyAmenities,
+      availability: availability,
+    });
+
+    let savedProperty = await newProperty.save();
+    response.status = 200;
+    response.message = "property added succesfully";
+    response.data = await formatMongoData(savedProperty);
+  } catch (error) {
+    response.message = "error message";
+    // response.data = {};
+    response.error = error.message;
+    console.log(error);
+    console.log("something went wrong: controller:  create property");
+  } finally {
+    return res.status(response.status).send(response);
+  }
+};
+
+module.exports.listProperty = async (req, res, skip= 0, limit = 10) => {
+  let response = serverResponse;
+  try {
+
+     skip = req.params.skip?req.params.skip:skip
+     limit = req.params.limit?req.params.limit:limit
+
+    
+    const properties =await Property.find({})
+      .skip(parseInt(skip))
+      .limit(parseInt(limit));
+
+    response.status = 200;
+    response.message = "property fetched succesfully";
+    response.data = await formatMongoData(properties);
+  } catch (error) {
+    response.message = "error message";
+    response.data = {};
+    response.error = error.message;
+    console.log(error);
+    console.log("something went wrong: controller:  fetching properties");
+  } finally {
+    return res.status(response.status).send(response);
+  }
+};
+
+module.exports.getPropertyById = async (req, res) => {
+    let response = serverResponse
+
+    try {
+        await checkObjectId(req.params.id)
+        const id = req.params.id
+
+        existingproperty = await Property.findById({_id: id})
+
+        if(!existingproperty) {
+            throw new Error ("property not found") 
+        }
+
+        response.status = 200
+        response.message = "property fetched successfully"
+        response.data = await formatMongoData(existingproperty)
+        
+    } catch (error) {
+        response.message = "error message"
+        response.error = error.message
+        response.data = {}
+        console.log("error with product controller: getPropertyById ")
+        console.log(error)
+    } finally {
+        return res.status(response.status).send(response)
+    }
+}
+
