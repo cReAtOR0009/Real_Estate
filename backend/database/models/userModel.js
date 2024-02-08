9aaf1a63a63669a7d7e2ec2d7b003e6384da4b38
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-02-08 12:28:28 +0100

    added virtual populate to user schema

--- backend/database/models/userModel.js
+++ backend/database/models/userModel.js
@@ -1,46 +1,57 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Property = require("./propertyModel");
 
 const purchasedPropertySchema = mongoose.Schema({
-    name: String,
-    id: mongoose.Schema.Types.ObjectId,
+  name: String,
+  id: mongoose.Schema.Types.ObjectId,
 });
 
-
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     firstName: String,
     lastName: String,
     email: String,
     phone: Number,
     password: String,
     nationality: String,
-    verified:{type:Boolean, default:false}, 
-    avatar:{type:String, default:"image"}, // "image" should be substituted for default image, if user is yet to upload image
-    purchasedProperty:  {
-        type: [purchasedPropertySchema],
+    verified: { type: Boolean, default: false },
+    avatar: { type: String, default: "image" }, // "image" should be substituted for default image, if user is yet to upload image
+    purchasedProperty: {
+        type: [{
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Property',
+        }],
         default: [],
-    },  
-    role: {  
-        type: String,   
-        enum: ['buyer', 'seller', 'agent'],
-        default:"buyer",
-        required: true,
+      },
+    role: {
+      type: String,
+      enum: ["buyer", "seller", "agent"],
+      default: "buyer",
+      required: true,
     },
     permissions: {
-        type: [String],
-        default:"level1",
+      type: [String],
+      default: "level1",
     },
-}, {
+  },
+  {
     timestamps: true,
     toObject: {
-        transform: (doc, ret, options) => {
-            ret.id = ret._id;
-            delete ret._id;
-            delete ret.__v;
-            delete ret.password
-            return ret;
+      transform: (doc, ret, options) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
+);
 
-        }
-    }
+userSchema.virtual("proprtyInfo", {
+  ref: "Property",
+  localField: "purchasedProperty",
+  foreignField: "_id",
+  justOne: true,
 });
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
