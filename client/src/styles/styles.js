91dcb6cb24996146c325fab3651c09c0bd707b1f
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-04 16:13:05 +0100

    created styles for house car property page

--- client/src/styles/styles.js
+++ client/src/styles/styles.js
@@ -1,22 +1,37 @@
-import { Container } from "postcss"
+import { Container } from "postcss";
 
 const styles = {
-    homeheader:"px-[10px] md:px-[80px] xl:px-[162px] ",
-    navContainer:"px-[16px] py-[30px]  md:px-[80px] md:py-[20px]  lg:pl[80px] lg:pt-[-100px] lg:pr-[80px]  xl:px-[400px] xl:py-[150px] color-[red]",
-    searchContainer:"px-[16px] py-[50px]  md:px-[80px] md:pt-[0px]  md:pb-[0px] lg:pl[80px] lg:pt-[-100px] lg:pr-[80px]  xl:px-[400px] xl:py-[150px] color-[red]",
-    TextContainer:" px-[16px] py-[50px]  md:p-[80px] md:pt-[100px] md:pr-[200px] md:pb-[130px] lg:pl[100px] lg:pt-[100px] lg:pr-[200px] lg:pb-[130px] xl:px-[400px] xl:py-[150px] color-[red]",
-    headerTextContainer:"px-[16px] py-[50px]  md:p-[80px] md:py-[10px] md:pr-[200px] md:pb-[130px] lg:py-[80px] lg:px-[80px]  xl:px-[400px] xl:py-[150px]",
-    heading:"text-[28px] md:text-[38px] lg:text-[48px] xl:text-[48px] lg:leading-[150%]",
-    paragraph:"text-[14px] md:text-[14px] lg:text-[16px] xl:text-[18px] text-Grey-60",
-    subContainer:"px-[5px] pt-[40px] md:px-[50px] md:py-[0px] lg:px-[80px] lg:mt-[10px] xl:px-[162px]",
-    houseCardPadding:"p-[20px] md:p-[30px] lg:pl[40px] xl:p-[40px]",
-    cardHeading:"text-[18px] md:text-[20px] lg:text-[24px] xl:text-[24px] text-white-90",
-    buttonPadding:"hover:bg-[#946cf9] px-[10px] py-[14px] lg:px-[24px] lg:py-[18px] text-[14px] md:text-[18px] rounded-[8px]",
-    purpleButton:"bg-Purple-60 rounded-[8px]",
-    blackButton:"border border-Grey-15  rounded-[8px]",
-    formContainer:"px-[20px] py:[20px] m-[10px] md:p-[30px] md:m-[30px] lg:p-[50px] lg:m-[80px] xl:p-[100px] xl:m-[100px]",
-    inputFied:"px-[16px] py-[20px] border border-Grey-15 rounded-[6px] bg-Grey-10 text-Grey-40 lg:rounded-[8px] text-[14px]",
-    inputFied2:"px-[36px] py-[20px] border border-Grey-15 rounded-[6px] bg-Grey-08 text-Grey-40 lg:rounded-[8px] text-[14px]"
-}
+  homeheader: "px-[10px] md:px-[80px] xl:px-[162px] ",
+  navContainer:
+    "px-[16px] py-[30px]  md:px-[80px] md:py-[20px]  lg:pl[80px] lg:pt-[-100px] lg:pr-[80px]  xl:px-[400px] xl:py-[150px] color-[red]",
+  searchContainer:
+    "px-[16px] py-[50px]  md:px-[80px] md:pt-[0px]  md:pb-[0px] lg:pl[80px] lg:pt-[-100px] lg:pr-[80px]  xl:px-[400px] xl:py-[150px] color-[red]",
+  TextContainer:
+    " px-[16px] py-[50px]  md:p-[80px] md:pt-[100px] md:pr-[200px] md:pb-[130px] lg:pl[100px] lg:pt-[100px] lg:pr-[200px] lg:pb-[130px] xl:px-[400px] xl:py-[150px] color-[red]",
+  headerTextContainer:
+    "px-[16px] py-[50px]  md:p-[80px] md:py-[10px] md:pr-[200px] md:pb-[130px] lg:py-[80px] lg:px-[80px]  xl:px-[400px] xl:py-[150px]",
+  heading:
+    "text-[28px] md:text-[38px] lg:text-[48px] xl:text-[48px] lg:leading-[150%]",
+  paragraph:
+    "text-[14px] md:text-[14px] lg:text-[16px] xl:text-[18px] text-Grey-60",
+  subContainer:
+    "px-[5px] pt-[40px] md:px-[50px] md:py-[0px] lg:px-[80px] lg:mt-[10px] xl:px-[162px]",
+  houseCardPadding: "p-[20px] md:p-[30px] lg:pl[40px] xl:p-[40px]",
+  houseCardForProperty: "mt-[120px] sm:mt-[120px] ",
+  cardHeading:
+    "text-[18px] md:text-[20px] lg:text-[24px] xl:text-[24px] text-white-90",
+  cardHeadingForProperty:
+    "text-[20px] md:text-[25px] lg:text-[25px] xl:text-[25px] text-white-90 text-center",
+  buttonPadding:
+    "hover:bg-[#946cf9] px-[10px] py-[14px] lg:px-[24px] lg:py-[18px] text-[14px] md:text-[18px] rounded-[8px]",
+  purpleButton: "bg-Purple-60 rounded-[8px]",
+  blackButton: "border border-Grey-15  rounded-[8px]",
+  formContainer:
+    "px-[20px] py:[20px] m-[10px] md:p-[30px] md:m-[30px] lg:p-[50px] lg:m-[80px] xl:p-[100px] xl:m-[100px]",
+  inputFied:
+    "px-[16px] py-[20px] border border-Grey-15 rounded-[6px] bg-Grey-10 text-Grey-40 lg:rounded-[8px] text-[14px]",
+  inputFied2:
+    "px-[36px] py-[20px] border border-Grey-15 rounded-[6px] bg-Grey-08 text-Grey-40 lg:rounded-[8px] text-[14px]",
+};
 
-export  {styles}
+export { styles };
