34cc6ae7ad2e6583b7d809438f1af990a6da00c1
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-05 15:49:33 +0100

    removed color from textContainer style

--- client/src/styles/styles.js
+++ client/src/styles/styles.js
@@ -7,7 +7,7 @@ const styles = {
   searchContainer:
     "px-[16px] py-[50px]  md:px-[80px] md:pt-[0px]  md:pb-[0px] lg:pl[80px] lg:pt-[-100px] lg:pr-[80px]  xl:px-[400px] xl:py-[150px] color-[red]",
   TextContainer:
-    " px-[16px] py-[50px]  md:p-[80px] md:pt-[100px] md:pr-[200px] md:pb-[130px] lg:pl[100px] lg:pt-[100px] lg:pr-[200px] lg:pb-[130px] xl:px-[400px] xl:py-[150px] color-[red]",
+    " px-[16px] py-[50px]  md:p-[80px] md:pt-[100px] md:pr-[200px] md:pb-[130px] lg:pl[100px] lg:pt-[100px] lg:pr-[200px] lg:pb-[130px] xl:px-[400px] xl:py-[150px]",
   headerTextContainer:
     "px-[16px] py-[50px]  md:p-[80px] md:py-[10px] md:pr-[200px] md:pb-[130px] lg:py-[80px] lg:px-[80px]  xl:px-[400px] xl:py-[150px]",
   heading:
