25301b4e692a0d81402bd595f315d5b14564eb96
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-06 02:28:40 +0100

    prettier

--- client/src/components/Amenity.jsx
+++ client/src/components/Amenity.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Amenity = ({ icon, text, available }) => (
   <p
-    className={`hover:scale-[1.2] flex px-[5px] py-[5px] sm:p-[10px] items-center m-[0px] gap-[2px] rounded-[28px] border border-solid border-Grey-15 bg-Purple-60 ${
+    className={`hover:scale-[1.2] transition-all flex px-[5px] py-[5px] sm:p-[10px] items-center m-[0px] gap-[2px] rounded-[28px] border border-solid border-Grey-15 bg-Purple-60 ${
       available
-        ? ""
+        ? ""  
         : "bg-[red] border border-solid border-[red] cursor-not-allowed"
     }`}
   >
