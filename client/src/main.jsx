db76c76f3482317900ae947be2662be058cfbb9a
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-06 04:45:00 +0100

    added PropertyProvider to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { NavigationProvider } from "./context/navigationContext.jsx";
 import { CartContextProvider } from "./context/cartContext.jsx";
+import { PropertyProvider } from "./context/PropertyContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <CartContextProvider>
     <NavigationProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <PropertyProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </PropertyProvider>
     </NavigationProvider>
   </CartContextProvider>
 );
