49274e5e1498a585340865e335d312a3a697dbdc
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-22 23:09:39 +0100

    reformatted ui

--- client/src/components/AdditionalFeatures.jsx
+++ client/src/components/AdditionalFeatures.jsx
@@ -4,7 +4,7 @@ import { styles } from "../styles/styles";
 
 const AdditionalFeatures = ({ name, description }) => {
   return (
-    <div className="flex flex-col px-[5px] py-[5px] sm:p-[10px] gap-[2px] rounded-[10px] border border-solid border-Grey-15 bg-Grey-20">
+    <div className="flex flex-col px-[5px] py-[5px] sm:p-[10px] gap-[2px]  border-l-2 border-Purple-60 bg-Grey-20">
       <h2 className={`text-[20px]`}>{name}</h2>
       <p className={`${styles.paragraph}`}>{description}</p>
     </div>
