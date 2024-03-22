b69600b01981d12fec7fd3356d67ca26c010b739
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-22 05:02:28 +0100

    prettier

--- client/src/layouts/pageLayout.jsx
+++ client/src/layouts/pageLayout.jsx
@@ -1,16 +1,16 @@
 import { Outlet } from "react-router-dom";
-import React from 'react'
-import  NavBar from "../components/NavBar";
+import React from "react";
+import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 const PageLayout = () => {
   return (
     <>
-       <NavBar />
-       <Outlet />
-       <Footer /> 
+      <NavBar />
+      <Outlet />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default PageLayout
+export default PageLayout;
