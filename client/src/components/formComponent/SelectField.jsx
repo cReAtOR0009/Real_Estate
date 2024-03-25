c4822ec0339cfcb0f9caacc46bd76e70eccdee0a
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-03-25 22:25:41 +0100

    added null option for options in put field select

--- client/src/components/formComponent/SelectField.jsx
+++ client/src/components/formComponent/SelectField.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const SelectField = ({ icon, label, options, styles, onChange }) => (
-    <div className="custom-select flex flex-1 items-center relative">
-      <label
-        htmlFor={label}
-        className="absolute left-3 text-[16px] text-White-99"
-      >
-        <img src={icon} alt="" />
-      </label>
-      <select
-        name={label}
-        id={label}
-        onChange={onChange}
-        className={`${styles} `}
-      >
-        {/* <option value="">{placeholder}</option> */}
-        {options.map((option, index) => (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
+  <div className="custom-select flex flex-1 items-center relative">
+    <label
+      htmlFor={label}
+      className="absolute left-3 text-[16px] text-White-99"
+    >
+      <img src={icon} alt="" />
+    </label>
+    <select
+      name={label}
+      id={label}
+      onChange={onChange}
+      className={`${styles} `}
+    >
+      {/* <option value="">{placeholder}</option> */}
+      <option value="">select {label}</option>
+      {options.map((option, index) => (
+        <option key={index} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  </div>
+);
 
-export default SelectField
+export default SelectField;
--- client/src/components/formComponent/SelectField2.jsx
+++ client/src/components/formComponent/SelectField2.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const SelectField2 = ({ label, options, styles, onChange }) => (
-    <div className="custom-select custom-selectPatch flex flex-col   flex-1 gap-[12px] md:gap-[16px] ">
-      <label for={label} className="text-[16px] text-White-99">
-        {label}
-      </label>
-      <select name={label} id={label} onChange={onChange} className={styles}>
-        {/* <option value="">{name}</option> */}
-        {options.map((option, index) => (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
+  <div className="custom-select custom-selectPatch flex flex-col   flex-1 gap-[12px] md:gap-[16px] ">
+    <label for={label} className="text-[16px] text-White-99">
+      {label}
+    </label>
+    <select name={label} id={label} onChange={onChange} className={styles}>
+      <option value="">select {label}</option>
+      {options.map((option, index) => (
+        <option key={index} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  </div>
+);
 
-export default SelectField2
+export default SelectField2;
