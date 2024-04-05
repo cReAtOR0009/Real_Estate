b42c81d053b88d78942179fc4b23e557c860ef08
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-04-05 18:42:08 +0100

    included border to journey component

--- client/src/components/smallcomponents/Journey.jsx
+++ client/src/components/smallcomponents/Journey.jsx
@@ -4,7 +4,7 @@ import { styles } from "../../styles/styles";
 const Journey = ({ paragraphText, HeaderText, buttonText }) => {
   return (
     <div
-      className={`px-[16px] py-[50px]  md:p-[80px] md:py-[10px] md:pb-[130px] lg:py-[80px] lg:px-[80px]  xl:px-[400px] xl:py-[150px]  flex flex-wrap md:flex-nowrap justify-between gap-[20px] md:gap-[auto] items-center propertyJourneybg`}
+      className={`px-[16px] py-[50px]  md:p-[80px] md:py-[10px] md:pb-[130px] lg:py-[80px] lg:px-[80px]  xl:px-[400px] xl:py-[150px]  flex flex-wrap md:flex-nowrap justify-between gap-[20px] md:gap-[auto] items-center propertyJourneybg border-y border-solid border-Grey-15`}
     >
       <div className=" ">
         <h1 className={styles.heading}>{HeaderText}</h1>
